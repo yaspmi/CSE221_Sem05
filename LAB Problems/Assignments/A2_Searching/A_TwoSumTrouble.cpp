677dac759580123c175f7fcc04371890415b00db
Mohosina Alam Mona <mohosina.alam.mona@g.bracu.ac.bd>
2026-03-04 23:45:05 +0600

    added comments for better understanding

--- LAB Problems/Assignments/A2_Searching/A_TwoSumTrouble.cpp
+++ LAB Problems/Assignments/A2_Searching/A_TwoSumTrouble.cpp
@@ -17,10 +17,13 @@ int main(){
         cin >> a;
         A.push_back(a);
     }
+    //remember the array is sorted
+    
     int l = 0, r = N - 1;
     while (l < r){
         long long sum = A[l] + A[r];
-        if(sum>S) r--;
+        if(sum>S) r--;         //if sum is bigger than the given sum
+                               //means the right pointer is too high needs to lower it
         else if (sum<S) l++;
         else if (sum == S){
             cout << l + 1 << " " << r + 1 << "\n";
