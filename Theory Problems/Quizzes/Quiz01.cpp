b653e6c67f81770cea7d888cec76ffda49a828a5
Mohosina Alam Mona <mohosina.alam.mona@g.bracu.ac.bd>
2026-03-09 14:52:26 +0600

    Q1

---
+++ Theory Problems/Quizzes/Quiz01.cpp
@@ -0,0 +1,30 @@
+//The problem was something like : The given array's elements are strictly
+//decreasing then strictly increasing. 
+//need to find the minimum element.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x;
+    cin>>x;
+    
+    if(x==0) return -1;
+    vector<int>A(x);
+    for(int i=0; i<x; i++){
+        cin>>A[i];
+    }
+    
+    int l=0, r=x-1;
+    while(l<r){
+        int mid=(l+r)/2;
+        if(A[mid]>A[mid+1]) l=mid+1;
+        else r=mid;
+    }
+    cout<<A[l];
+    return 0;
+
+}
