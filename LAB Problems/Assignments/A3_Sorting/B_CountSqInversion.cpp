5f6578a3e7c279e16ace5652af0c26147dde6876
Mohosina Alam Mona <mohosina.alam.mona@g.bracu.ac.bd>
2026-03-11 01:54:40 +0600

    ...

---
+++ LAB Problems/Assignments/A3_Sorting/B_CountSqInversion.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long invCount = 0;
+vector<int> merSort(const vector<int>&A, int l, int r);
+vector<int> mergeArrays(const vector<int>&a, const vector<int>&b);
+int main(){
+ int x;
+ cin>>x;
+ vector<int>A(x);
+ 
+ for(int i=0; i<x; i++) cin>>A[i];
+ A=merSort(A, 0, x-1);
+ cout<<invCount<<"\n";
+ return 0;
+}
+
+vector<int> mergeArrays(const vector<int>&a, const vector<int>&b){
+
+    int n=a.size();
+    int m=b.size();
+    int i=0, j=0,k=0;
+
+    vector<int>c(n+m);
+
+    for(; j<m; j++){
+      long long sq = 1LL * b[j] * b[j];
+      int pos = upper_bound(a.begin(), a.end(), sq) - a.begin();
+      invCount += (n - pos);
+    }
+    
+    i=0;
+    j=0;
+    while(i<n && j<m){
+       if(a[i]<=b[j]){
+         c[k++]=a[i++];
+       }
+       else {
+         c[k++]=b[j++];
+       }
+    }
+
+    while(i<n) c[k++]=a[i++];
+    while(j<m) c[k++]=b[j++];
+
+    return c;
+
+}
+
+vector<int> merSort(const vector<int>&A, int l, int r){
+    if(l > r) return {};
+    if(l == r) return vector<int>{A[l]};
+    int mid= (l+r)/2;
+    return mergeArrays(merSort(A, l, mid), merSort(A, mid+1, r));      
+}
